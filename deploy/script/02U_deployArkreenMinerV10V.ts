51716867209a886d86d4ff885ab41e1b1903446d
Lu-Derik <ludaoru@gmail.com>
2022-12-07 11:50:45 +0800

    gameMiner in Matic Mainnet is upgraded to ArkreenMinerV10V.sol

--- deploy/script/02U_deployArkreenMinerV10V.ts
+++ deploy/script/02U_deployArkreenMinerV10V.ts
@@ -5,6 +5,8 @@ import { CONTRACTS } from "../constants";
 import { ArkreenMinerV10V__factory } from "../../typechain";
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+
+/*
   // Check following address
   let AKREToken_ADDRESS
   let MANAGER_ADDRESS
@@ -25,6 +27,7 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     console.log("AKREToken_ADDRESS or MANAGER_ADDRESS not set, Wrong Network Name")
     return
   }
+*/
 
   console.log("Deploying ArkreenMiner...");  
   const { deployments, getNamedAccounts } = hre;
@@ -46,16 +49,18 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
 //  const ArkreenMiner_Upgrade = await ArkreenMinerFactory.deploy();
 //  await ArkreenMiner_Upgrade.deployed();
 
-  if(hre.network.name === 'matic_test') {
+  if(hre.network.name === 'matic_test'  || hre.network.name === 'matic') {
 //  const MINER_PROXY_ADDRESS = "0xC4f795514586275c799729aF5AE7113Bdb7ccc86"        // game miner in matic test net
-    const MINER_PROXY_ADDRESS = "0x40e0a167be5c8427513394914a15e2be3aca573d"        // game miner in matic test net for AREC 
+//  const MINER_PROXY_ADDRESS = "0x40e0a167be5c8427513394914a15e2be3aca573d"        // game miner in matic test net for AREC 
+    const MINER_PROXY_ADDRESS = "0xAc4da3681e51278f82288617c7185a7a119E5b7B"        // game miner in matic mainnet
 
 //  const NEW_IMPLEMENTATION =  "0x4EDe87d416e872376583E793ac26526c535267C5"        // Wrong
 //  const NEW_IMPLEMENTATION =  "0x7693ad7e3a69b241322094b14fcaec233afb3e56"        // original 
 //  const NEW_IMPLEMENTATION =  "0x16a427a1a2012fdde0ccad2664d5f2981d52a2d2"        // restored Wrong
 //  const NEW_IMPLEMENTATION =  "0x544dc72e13904bda9719c3511b7c2f1e616488d1"        // V10X // adding remote miner
 //  const NEW_IMPLEMENTATION =  "0x6ca97ce7fb9d77f2eb961957791acdab5484f936"        // V10X // adding GetMinerInfo
-    const NEW_IMPLEMENTATION =  "0xbe39aa3fa24dd8d637c0a0965cb7fb91302d0634"        // V10V // adding GetMinerInfo
+//  const NEW_IMPLEMENTATION =  "0xbe39aa3fa24dd8d637c0a0965cb7fb91302d0634"        // V10V // upgrade gameMiner of AREC testing
+    const NEW_IMPLEMENTATION =  "0x8aa572ee9c7991dc059a2ae18844858b1eb274f0"        // V10V // upgrade gameMiner of in MATIC mainnet
 
     const [deployer] = await ethers.getSigners();
     const ArkreenMinerFactory = ArkreenMinerV10V__factory.connect(MINER_PROXY_ADDRESS, deployer);
