2ec92f6e1965dba78a9cf10f28e952cdea645bbc
Lu-Derik <ludaoru@gmail.com>
2023-04-06 17:22:16 +0800

    USDC/USDT price is corrected

--- deploy/script/14I_deploy_ArkreenRECBank.ts
+++ deploy/script/14I_deploy_ArkreenRECBank.ts
@@ -60,10 +60,10 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
       WMATIC_ADDRESS    = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270"          // WMATIC address
       AKRE_ADDRESS      = "0x21b101f5d61a66037634f7e1beb5a733d9987d57"          // tAKRE address
 
-      USDC_PRICE =      expandTo15Decimals(2500)                              // 2.5 USDC
-      USDT_PRICE =      expandTo15Decimals(2500)                              // 2.5 USDT
-      MATIC_PRICE=      expandTo15Decimals(2500)                              // 2.5 MATIC, 
-//    AKRE_PRICE =      expandTo18Decimals(200)                               // 200 AKRE
+      USDC_PRICE =      BigNumber.from(25).mul(BigNumber.from(10).pow(5))       // 2.5 USDC
+      USDT_PRICE =      BigNumber.from(25).mul(BigNumber.from(10).pow(5))       // 2.5 USDT
+      MATIC_PRICE=      expandTo15Decimals(2500)                                // 2.5 MATIC, 
+//    AKRE_PRICE =      expandTo18Decimals(200)                                 // 200 AKRE
     } 
     
     const [deployer, controller] = await ethers.getSigners();
@@ -79,21 +79,23 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     await setForwarderTrx.wait()
 */
     // 2023/04/05
-    const addNewARTTRx = await ArkreenRECBankFactory.addNewART(HART_REC as string , ART_CONTROLLER as string);
-    await addNewARTTRx.wait()
+//    const addNewARTTRx = await ArkreenRECBankFactory.addNewART(HART_REC as string , ART_CONTROLLER as string);
+//    await addNewARTTRx.wait()
 
     // Called by controller, Account 2
+    // 2023/04/06
     const changeSalePriceUSDSC = await ArkreenRECBankFactory.connect(controller).changeSalePrice(HART_REC as string, 
                                     USDC_ADDRESS as string, USDC_PRICE as BigNumber)
     await changeSalePriceUSDSC.wait()
-    
+
+    // 2023/04/06
     const changeSalePriceUSDST = await ArkreenRECBankFactory.connect(controller).changeSalePrice(HART_REC as string, 
                                     USDT_ADDRESS as string, USDT_PRICE as BigNumber)
     await changeSalePriceUSDST.wait()
 
-    const changeSalePriceMATIC = await ArkreenRECBankFactory.connect(controller).changeSalePrice(HART_REC as string, 
-                                    WMATIC_ADDRESS as string, MATIC_PRICE as BigNumber)
-    await changeSalePriceMATIC.wait()   
+//    const changeSalePriceMATIC = await ArkreenRECBankFactory.connect(controller).changeSalePrice(HART_REC as string, 
+//                                    WMATIC_ADDRESS as string, MATIC_PRICE as BigNumber)
+//    await changeSalePriceMATIC.wait()   
 
       // tAKRE removed    
 //    const changeSalePriceAKRE = await ArkreenRECBankFactory.changeSalePrice(HART_REC as string, 
@@ -121,7 +123,11 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
 
 // 2023/04/05
 // yarn deploy:matic:ArtBankI
-// Action: addNewART,  changeSalePrice(USDC), changeSalePrice(USDT), changeSalePrice(WMATIC)      
+// Action: addNewART,  changeSalePrice(USDC), changeSalePrice(USDT), changeSalePrice(WMATIC)   
+
+// 2023/04/06: Correct USDC/USDT price
+// yarn deploy:matic:ArtBankI
+// Action: changeSalePrice(USDC), changeSalePrice(USDT)
 
 func.tags = ["ArtBankI"];
 
