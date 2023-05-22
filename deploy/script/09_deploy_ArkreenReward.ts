81beedfeaa261bcc77a8ea11e36cc7f49c9c2c83
Lu-Derik <ludaoru@gmail.com>
2023-05-22 19:34:46 +0800

    Reward contract deployed

--- deploy/script/09_deploy_ArkreenReward.ts
+++ deploy/script/09_deploy_ArkreenReward.ts
@@ -4,11 +4,15 @@ import { DeployFunction } from "hardhat-deploy/types";
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
 
-    const arkReward = "ArkreenRewardUpgradeable"
+    const arkReward = "ArkreenReward"
 
-     // Need to check and update !!!!!
-    const AKRE_TOKEN_ADDRESS    = "0x960c67b8526e6328b30ed2c2faea0355beb62a83"  
-    const VALIDATOR_ADDRESS     = "0x25C3f0Ea82E318b3488adfB3f4cbE76b49508B1c"  
+    // Need to check and update !!!!!
+//  const AKRE_TOKEN_ADDRESS    = "0x960c67b8526e6328b30ed2c2faea0355beb62a83"  
+//  const VALIDATOR_ADDRESS     = "0x25C3f0Ea82E318b3488adfB3f4cbE76b49508B1c"  
+
+    // 2023/05/16: Open testnet
+    const AKRE_TOKEN_ADDRESS    = "0x21B101f5d61A66037634f7e1BeB5a733d9987D57"
+    const VALIDATOR_ADDRESS     = "0x741e2403b63a41188e72dc3897d730a746d9181e"
 
     const { deployments, getNamedAccounts } = hre;
     const { deploy } = deployments;
@@ -32,6 +36,11 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
 
     console.log("ArkreenReward deployed to %s: ", hre.network.name, ArkreenReward.address);
 };
+
+// 2023/05/16: Open testnet
+// yarn deploy:matic:arkReward  
+// Proxy:           0x44082E783bA0aBB62975e77E48aF355B2bF3d440         
+// Implementaion:   0x2f47c3346300ca8c04e0af15c527216f5363910c
 
 func.tags = ["arkReward"];
 
