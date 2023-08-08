10d39413589cce6de599261fe6d971b2ac8b1da9
Lu-Derik <ludaoru@gmail.com>
2023-08-08 18:57:49 +0800

    climateBuilder is added to ART token in simu mode

--- deploy/script/06I_deploy_ArkreenRECToken.ts
+++ deploy/script/06I_deploy_ArkreenRECToken.ts
@@ -9,6 +9,26 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
 
   console.log("Update ArkreenRECToken: ", CONTRACTS.RECToken, deployer.address);  
 
+  if(hre.network.name === 'matic_test') {
+    const ART_ADDRESS    = "0xb0c9dD915f62d0A37792FD2ce497680E909D8c0F"        // ART simu: Need to check
+    // const ART_ADDRESS = "0x0999afb673944a7b8e1ef8eb0a7c6ffdc0b43e31"        // HART simu
+    // const AREC_ISSUE_ADDRESS  = "0xFedD52848Cb44dcDBA95df4cf2BCBD71D58df879"
+    // const RECTOKEN_ADDRESS    = "0xb0c9dD915f62d0A37792FD2ce497680E909D8c0F"  // ART simu: Need to check
+    const BUILDER_ADDRESS   = "0xA05A9677a9216401CF6800d28005b227F7A3cFae"       // Action Builder address: Simu
+
+    const ArkreenRECTokenFactory = ArkreenRECToken__factory.connect(ART_ADDRESS, deployer);
+
+    // Set ArkreenBuilder address to the ART/HART token contract
+    const setClimateBuilderTx = await ArkreenRECTokenFactory.setClimateBuilder( BUILDER_ADDRESS as string)
+    await setClimateBuilderTx.wait()
+
+    console.log("ArkreenRECToken Tx:", setClimateBuilderTx)
+    console.log(" Set ArkreenBuilder address to the ART/HART token contract: ", hre.network.name, BUILDER_ADDRESS, ART_ADDRESS );
+    
+    //const updateTx = await  ArkreenRECTokenFactory.setClimateBuilder(AREC_ISSUE_ADDRESS)
+    //await updateTx.wait()
+  }
+  
   if(hre.network.name === 'matic') {
     const RECTOKEN_ADDRESS    = "0x58E4D14ccddD1E993e6368A8c5EAa290C95caFDF"        // Need to check
     const AREC_ISSUE_ADDRESS  = "0xFedD52848Cb44dcDBA95df4cf2BCBD71D58df879"
@@ -25,6 +45,9 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
 
 // 2023/05/10: Call setIssuerREC to update AREC_ISSUE_ADDRESS: 0xFedD52848Cb44dcDBA95df4cf2BCBD71D58df879
 // yarn deploy:matic:RECTokenI
+
+// 2023/08/08: Call setClimateBuilder to update climateBuilder: 0xA05A9677a9216401CF6800d28005b227F7A3cFae
+// yarn deploy:matic_test:RECTokenI
 
 func.tags = ["RECTokenI"];
 
