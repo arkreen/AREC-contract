4355678045ea01586c930a7072553ba687ca6bbd
Lu-Derik <ludaoru@gmail.com>
2023-08-31 17:53:02 +0800

    Certify for celo added

---
+++ deploy/confirmIssuance_celo.ts
@@ -0,0 +1,116 @@
+import { ethers } from "hardhat";
+import * as fs from "fs"
+import { ArkreenRECIssuance__factory } from "../typechain";
+import { RECDataStructOutput } from "../typechain/contracts/ArkreenRECIssuance";
+
+export enum REC_STATUS {
+  Pending,            // 0
+  Rejected,           // 1
+  Cancelled,          // 2
+  Certified,          // 3
+  Retired,            // 4
+  Liquidized          // 5
+}
+
+interface confirmInfo {
+  counter:    string
+  SerialNo:   string
+}
+
+function getConfirmInfo(): confirmInfo {
+  const counterFile = './deploy/confirmCounter_celo.txt'
+  const confirmInfoStr = fs.readFileSync(counterFile,'ascii')
+  const confirmInfoJson = JSON.parse(confirmInfoStr)
+  return confirmInfoJson
+}
+
+function setConfirmInfo(confirmInfoJson: confirmInfo) {
+  const counterFile = './deploy/confirmCounter_celo.txt'
+  fs.writeFileSync(counterFile, JSON.stringify(confirmInfoJson))
+}
+
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+async function main() {
+
+  const [owner, ConfirmAccount] = await ethers.getSigners();
+  console.log('Account',  owner.address, ConfirmAccount.address)
+
+  let confirmInfoJson: confirmInfo = getConfirmInfo()
+
+  const ISSUANCE_ADDRESS = "0x66e9c20DE3711e7C8c886d461aACd6E092E161BE"       // Need to check
+  const arkreenRECIssuance = ArkreenRECIssuance__factory.connect(ISSUANCE_ADDRESS, ConfirmAccount);
+
+  // Get latest AREC issuance status
+//  const totalARECNumber =  await arkreenRECIssuance.totalSupply()
+//  const ARECNftInfo: RECDataStructOutput = await arkreenRECIssuance.getRECData(totalARECNumber)
+
+//  let confirmInfoJson: confirmInfo = {  counter: totalARECNumber.toString().padStart(4,'0'),
+//                                        SerialNo: ARECNftInfo.serialNumber}
+//  setConfirmInfo(confirmInfoJson)                                        
+
+  console.log("Current Confirmation:", confirmInfoJson, (new Date()).toLocaleString())
+  while(true) {
+    let totalARECNumber
+    while(true) {
+      try{
+        totalARECNumber = await arkreenRECIssuance.totalSupply()
+        break
+      } catch (error) {
+        console.log("Error happened:", error, (new Date()).toLocaleString())
+      }
+    }
+    const startTokenID = Number(confirmInfoJson.counter)
+    let NewSerialNo = Number(confirmInfoJson.SerialNo)
+
+    let moveSNForward = true
+
+    console.log("Current Confirmation:", confirmInfoJson, (new Date()).toLocaleString(), startTokenID, totalARECNumber)
+    for(let index = (startTokenID+1); index <= totalARECNumber.toNumber(); index++) {      // Id start from 1
+      try {
+        const ARECNftInfo: RECDataStructOutput = await arkreenRECIssuance.getRECData(index)
+        // console.log('AAAAAAAAAAAAA', moveSNForward, NewSerialNo, index, ARECNftInfo )
+        
+        if(ARECNftInfo.status == REC_STATUS.Pending) {
+          if (ARECNftInfo.cID.length >= 20) {
+            NewSerialNo = NewSerialNo +1
+            const NewSerialNoString = "REC_CELO_" + NewSerialNo.toString().padStart(8,'0')
+            await arkreenRECIssuance.certifyRECRequest(index, NewSerialNoString)
+
+            confirmInfoJson.SerialNo = NewSerialNo.toString().padStart(8,'0')
+            if(moveSNForward) {
+              confirmInfoJson.counter = index.toString().padStart(4,'0')
+            }
+
+            setConfirmInfo(confirmInfoJson)
+            console.log("Updated Confirmation: BBBBBBBBBBBB", moveSNForward, index, NewSerialNo, confirmInfoJson, (new Date()).toLocaleString())
+          } else (
+            moveSNForward = false
+          )
+        } else if(ARECNftInfo.status >= REC_STATUS.Certified && moveSNForward) {
+          confirmInfoJson.counter = index.toString().padStart(4,'0')
+          if (ARECNftInfo.serialNumber >= confirmInfoJson.SerialNo) {
+            confirmInfoJson.SerialNo = ARECNftInfo.serialNumber
+          }
+
+          setConfirmInfo(confirmInfoJson)
+          console.log("Updated Confirmation: CCCCCCCCCCCCC", moveSNForward, index, NewSerialNo, confirmInfoJson, (new Date()).toLocaleString())
+        }
+
+        await sleep(5*1000)  // sleep 1 minute
+      }
+      catch (error) {
+        console.log("Error happened:", error, (new Date()).toLocaleString())
+        break
+      }
+    }
+    await sleep(60*1000)  // sleep 1 minute
+  }
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
