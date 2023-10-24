c80d634dc2461376fb846d34b5c5a972765a413c
Lu-Derik <ludaoru@gmail.com>
2023-10-24 18:14:30 +0800

    Testing

--- deploy/imageTest.ts
+++ deploy/imageTest.ts
@@ -1,113 +1,85 @@
 import { ethers } from "hardhat";
 import * as fs from "fs"
-import { ArkreenRECIssuance__factory } from "../typechain";
-import { RECDataStructOutput } from "../typechain/contracts/ArkreenRECIssuance";
+import { GreenBTC__factory } from "../typechain";
+import { constants, BigNumber, utils } from 'ethers'
 
-export enum REC_STATUS {
-  Pending,            // 0
-  Rejected,           // 1
-  Cancelled,          // 2
-  Certified,          // 3
-  Retired,            // 4
-  Liquidized          // 5
+import { getGreenBitcoinDigest  } from "../test/utils/utilities";
+import { ecsign } from 'ethereumjs-util'
+
+export const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+export function expandTo6Decimals(n: number): BigNumber {
+  return BigNumber.from(n).mul(BigNumber.from(10).pow(6))
 }
-
-interface confirmInfo {
-  counter:    string
-  SerialNo:   string
-}
-
-function getConfirmInfo(): confirmInfo {
-  const counterFile = './deploy/confirmCounter_celo.txt'
-  const confirmInfoStr = fs.readFileSync(counterFile,'ascii')
-  const confirmInfoJson = JSON.parse(confirmInfoStr)
-  return confirmInfoJson
-}
-
-function setConfirmInfo(confirmInfoJson: confirmInfo) {
-  const counterFile = './deploy/confirmCounter_celo.txt'
-  fs.writeFileSync(counterFile, JSON.stringify(confirmInfoJson))
-}
-
-const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
 async function main() {
 
   const [owner, ConfirmAccount] = await ethers.getSigners();
   console.log('Account',  owner.address, ConfirmAccount.address)
 
-  let confirmInfoJson: confirmInfo = getConfirmInfo()
+  const GREEN_BTC_ADDRESS = "0x80218fCa50363E3B31A93bB29bEe7ABafc157137"       // Need to check
+  const greenBTC = GreenBTC__factory.connect(GREEN_BTC_ADDRESS, owner);   
 
-  const ISSUANCE_ADDRESS = "0x66e9c20DE3711e7C8c886d461aACd6E092E161BE"       // Need to check
-  const arkreenRECIssuance = ArkreenRECIssuance__factory.connect(ISSUANCE_ADDRESS, ConfirmAccount);
+  const USDC_ADDRESS = "0x0FA8781a83E46826621b3BC094Ea2A0212e71B23"           // Need to check
+  const privateKeyRegister = process.env.REGISTER_TEST_PRIVATE_KEY as string
 
-  // Get latest AREC issuance status
-//  const totalARECNumber =  await arkreenRECIssuance.totalSupply()
-//  const ARECNftInfo: RECDataStructOutput = await arkreenRECIssuance.getRECData(totalARECNumber)
-
-//  let confirmInfoJson: confirmInfo = {  counter: totalARECNumber.toString().padStart(4,'0'),
-//                                        SerialNo: ARECNftInfo.serialNumber}
-//  setConfirmInfo(confirmInfoJson)                                        
-
-  console.log("Current Confirmation:", confirmInfoJson, (new Date()).toLocaleString())
-  while(true) {
-    let totalARECNumber
-    while(true) {
-      try{
-        totalARECNumber = await arkreenRECIssuance.totalSupply()
-        break
-      } catch (error) {
-        console.log("Error happened:", error, (new Date()).toLocaleString())
-      }
-    }
-    const startTokenID = Number(confirmInfoJson.counter)
-    let NewSerialNo = Number(confirmInfoJson.SerialNo)
-
-    let moveSNForward = true
-
-    console.log("Current Confirmation:", confirmInfoJson, (new Date()).toLocaleString(), startTokenID, totalARECNumber)
-    for(let index = (startTokenID+1); index <= totalARECNumber.toNumber(); index++) {      // Id start from 1
-      try {
-        const ARECNftInfo: RECDataStructOutput = await arkreenRECIssuance.getRECData(index)
-        // console.log('AAAAAAAAAAAAA', moveSNForward, NewSerialNo, index, ARECNftInfo )
-        
-        if(ARECNftInfo.status == REC_STATUS.Pending) {
-          if (ARECNftInfo.cID.length >= 20) {
-            NewSerialNo = NewSerialNo +1
-            const NewSerialNoString = "REC_CELO_" + NewSerialNo.toString().padStart(8,'0')
-            await arkreenRECIssuance.certifyRECRequest(index, NewSerialNoString)
-
-            confirmInfoJson.SerialNo = NewSerialNo.toString().padStart(8,'0')
-            if(moveSNForward) {
-              confirmInfoJson.counter = index.toString().padStart(4,'0')
-            }
-
-            setConfirmInfo(confirmInfoJson)
-            console.log("Updated Confirmation: BBBBBBBBBBBB", moveSNForward, index, NewSerialNo, confirmInfoJson, (new Date()).toLocaleString())
-          } else (
-            moveSNForward = false
-          )
-        } else if(ARECNftInfo.status >= REC_STATUS.Certified && moveSNForward) {
-          console.log("Updated Confirmation: XXXXXXXXXXXX", moveSNForward, index, NewSerialNo, confirmInfoJson)
-
-          confirmInfoJson.counter = index.toString().padStart(4,'0')
-          if (Number(ARECNftInfo.serialNumber) > Number(confirmInfoJson.SerialNo)) {
-            confirmInfoJson.SerialNo = ARECNftInfo.serialNumber
-          }
-
-          setConfirmInfo(confirmInfoJson)
-          console.log("Updated Confirmation: CCCCCCCCCCCCC", moveSNForward, index, NewSerialNo, confirmInfoJson, (new Date()).toLocaleString())
-        }
-
-        await sleep(5*1000)  // sleep 1 minute
-      }
-      catch (error) {
-        console.log("Error happened:", error, (new Date()).toLocaleString())
-        break
-      }
-    }
-    await sleep(60*1000)  // sleep 1 minute
+  // Normal: authMintGreenBTCWithApprove   
+  let greenBTCInfo =  {
+    height: BigNumber.from(10345),
+    ARTCount: expandTo6Decimals(100),  // 0.1 HART
+    beneficiary: owner.address,
+    greenType: 1,
+    blockTime: 'Apr 26, 2009 10:25 PM UTC',
+    energyStr: '0.123 MWh'
   }
+
+  console.log("111111111111111111111")                          
+
+  const badgeInfo =  {
+    beneficiary:    owner.address,
+    offsetEntityID: 'Owner1',
+    beneficiaryID:  'Tester',
+    offsetMessage:  "Just Testing"
+  }    
+
+  const approve = { 
+    height:       greenBTCInfo.height,
+    energyStr:    greenBTCInfo.energyStr,
+    artCount:     greenBTCInfo.ARTCount,
+    blockTime:    greenBTCInfo.blockTime,
+    beneficiary:  greenBTCInfo.beneficiary,
+    greenType:    greenBTCInfo.greenType
+  }
+
+  console.log("222222222222222222222222222", greenBTC.address, approve )   
+
+  const register_digest =  utils.keccak256(
+    utils.defaultAbiCoder.encode(
+      [ 'uint256', 'string', 'uint256', 'string', 'address', 'uint8'],
+      [ approve.height, approve.energyStr, approve.artCount, approve.blockTime, approve.beneficiary, approve.greenType]
+    )
+  )
+  console.log("333333333333333333333")   
+
+  let amountPay = expandTo6Decimals(100).mul(200).div(expandTo6Decimals(1000))
+
+//  const {v,r,s} = ecsign( Buffer.from(register_digest.slice(2), 'hex'), 
+//                          Buffer.from(privateKeyRegister.slice(2), 'hex'))   
+                          
+  const {v,r,s} = {v:1, r:register_digest, s:register_digest}       // Fake                    
+       
+  console.log("9999999999999999999999999")                          
+  try {
+    const tx  = await greenBTC.authMintGreenBTCWithApprove( greenBTCInfo, {v,r,s}, badgeInfo, 
+            {token: USDC_ADDRESS, amount: amountPay}, constants.MaxUint256)   
+    
+    console.log("AAAAAAAAAAAAA", tx)
+
+  } catch (error) {
+    console.log("Error happened:", error, (new Date()).toLocaleString())
+  }
+
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
