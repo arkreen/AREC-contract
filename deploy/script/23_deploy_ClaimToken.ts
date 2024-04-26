518f5105c1b0880697b1220dfef9e7eb4eea49ff
Lu-Derik <ludaoru@gmail.com>
2024-04-26 11:26:38 +0800

    1. ClaimToken is deployed on Polygon mainnet.

--- deploy/script/23_deploy_ClaimToken.ts
+++ deploy/script/23_deploy_ClaimToken.ts
@@ -9,7 +9,7 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
 
-    const defaultGasPrice = (hre.network.name === 'matic_test') ? BigNumber.from(3_000_000_000) : BigNumber.from(100_000_000_000)
+    const defaultGasPrice = (hre.network.name === 'matic_test') ? BigNumber.from(3_000_000_000) : BigNumber.from(160_000_000_000)
 
     let tokenART
     let manager
@@ -20,7 +20,12 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
       tokenART = "0x615835Cc22064a17df5A3E8AE22F58e67bCcB778"
       manager = "0x8C4D62477F70C7Ea628B52dbF37DcC2E5e4043E2"
       from = "0x364a71eE7a1C9EB295a4F4850971a1861E9d3c7D" 
-    }
+    } else if(hre.network.name === 'matic')  {
+      // 2024/04/26: Polygon mainnet             
+      tokenART = "0x58E4D14ccddD1E993e6368A8c5EAa290C95caFDF"
+      manager = "0x1E1A152D1C77A16863e97DAf18E99f85a5F0a605"
+      from = "0x1249B1eABcAE642CF3Cb1e512a0075CEe92769BE" 
+    } 
 
     console.log("Deploying: ", "ClaimToken", deployer);  
     const claimToken = await deploy("ClaimToken", {
@@ -46,6 +51,11 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
 // yarn deploy:matic_test:ClaimToken    : Amoy testnet (Dev Anv)
 // Proxy:                 0x280a7c4E032584F97E84eDd396a00799da8D061A 
 // Implementaion:         0x6cc9F08e0516f684E235e2F2Af896ca56141d930
+
+// 2024/04/16
+// yarn deploy:matic:ClaimToken    : Polygon miannet
+// Proxy:                 0x62DD5611aa2708c3989DD33EBf913Eb428B28302 
+// Implementaion:         0x17Eea195A8466a1f820203a35e08047C957f55ab
 
 func.tags = ["ClaimToken"];
 
