0edc44335d1041c1856ca7f71d7151d6ce657f00
Lu-Derik <ludaoru@gmail.com>
2025-01-09 17:31:39 +0800

    1. GreenBTC2S is changed to support Node feature.

--- contracts/GreenBTCV2/GreenBTC2S.sol
+++ contracts/GreenBTCV2/GreenBTC2S.sol
@@ -35,8 +35,18 @@ contract GreenBTC2S is
         uint96     amountDroped;
     }
 
+    struct NodeInfo {
+        address     owner;
+        uint24      nodeId;
+        uint8       percentage;
+        uint64      amountEnergy;
+    }
+
     // keccak256("makeGreenBoxLucky(uint256 domainID,uint256 boxSteps,address greener,uint256 nonce,uint256 deadline)");
     bytes32 public constant LUCKY_TYPEHASH = 0x6A10D25EB5A7B84EB21D26AF2DB8C23A1FB80647769A40DA523EDDCFFC172A10;  
+
+    // keccak256("buyNode(address owner,uint256 nodeId,uint256 percentage,uint256 amountEnergy)");
+    bytes32 public constant BUY_NODE_TYPEHASH = 0x6A10D25EB5A7B84EB21D26AF2DB8C23A1FB80647769A40DA523EDDCFFC172A10;  
 
     address public kWhToken;
     address public domainManager;
@@ -47,7 +57,7 @@ contract GreenBTC2S is
     // x: MSB0:1; y: MSB1:1; w: MSB2:1; h: MSB3:1; boxTop:MSB4:4
     // chance1: MSB8:2; chance2: MSB10:2; chance3: MSB12:2; chance4: MSB14:2
     // ratio1: MSB16:2; ratio2: MSB18:2; ratio3: MSB20:2; ratio4: MSB22:2
-    // decimal:MSB24:1, reserved: MSB25:3, boxGreened: MSB28:4; 
+    // decimal:MSB24:1, nodeId: MSB25:3, boxGreened: MSB28:4; 
     mapping (uint256 => bytes32) public domains;
 
     // blockHeight: MSB0:4; domainId: MSB4:2; boxStart: MSB6:4; boxAmount: MSB10: 2; Owner address: MSB12:20
@@ -61,10 +71,17 @@ contract GreenBTC2S is
     LuckyFundInfo public luckyFundInfo;
     address public luckyManager;
 
+    mapping(uint24 => NodeInfo) public nodeInfo;            // All nodes sold
+    uint32 public nodeSold;                                 // 
+    bytes internal allNodeIds;
+
     event DomainRegistered(uint256 domainID, bytes32 domainInfo);
     event DomainGreenized(address gbtcActor, uint256 actionNumber, uint256 blockHeight, uint256 domainID, uint256 boxStart, uint256 boxNumber);
+    event DomainGreenizedNode(address gbtcActor, uint256 nodeId, address nodeOwner, uint256 percentage,
+                                        uint256 actionNumber, uint256 blockHeight, uint256 domainID, uint256 boxStart, uint256 boxNumber);
     event DomainGreenizedLucky(address gbtcActor, uint256 actionNumber, uint256 blockHeight, uint256 domainID, uint256 boxStart, uint256 boxNumber, uint256 nonce);
     event KWhDeposit(uint256 amount);
+    event BuyNode(uint256 nodeId, address owner, uint256 percentage, uint256 amountEnergy);
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -128,14 +145,16 @@ contract GreenBTC2S is
      * @param domainID the ID of the domain to be registered
      * @param domainInfo config info of the domain, formated as: 
      *  x: B0:1; y: B1:1; w: B2:1; h: B3:1; (x,y,w,h) define the position of the domain in map, 1 unit = 16 blocks
-     *  boxCap: B5:3, the box cap of the domain
+     *  boxCap: B4:4, the box cap of the domain
      *  chance1: B8:2; chance2: B10:2; chance3: B12:2; chance4: B14:2, the chance of the prize without lock , 500 means 5% 
      *  ratio1: B16:2; ratio1: B18:2; ratio1: B20:2; ratio1: B22:2, the chance of the prize with lock, 1500 means 15% 
-     *  decimal:MSB24:1, how much kWh token for 1 box, the exponent of power; reserved: MSB25:7
+     *  decimal: B24:1, how much kWh token for 1 box, the exponent of power; 
+     *  nodeId: B25:3, the node Id of the domain linked
+     *  boxGreened: B28:4; ( reserved: B28-B31 should be 0 in command)
      *  domainInfo is saved in converted format
      */
     function registerDomain (uint256 domainID, bytes32 domainInfo) public onlyManager {
-        // require (uint256(domains[domainID]) == 0, "GBC2: Wrong Domain ID");
+        require (uint256(domains[domainID]) == 0, "GBC2: Wrong Domain ID");
 
         uint256 ratioSum;
         uint256 domainInfoSaved; 
@@ -148,9 +167,36 @@ contract GreenBTC2S is
         require (ratioSum < 65536, "GBC2: Wrong Chance");
 
         domainInfoSaved += ((uint256(domainInfo) >> 192) << 192);
-        domainInfoSaved += uint64(uint256(domainInfo));
+        domainInfoSaved += uint64((uint256(domainInfo) >> 32) <<32);        // Clear last 4 bytes
         domains[domainID] = bytes32(domainInfoSaved);
         emit DomainRegistered(domainID, domainInfo);
+    }
+
+    /**
+     * @dev Buy Node. Node information are signed by manager
+     * @param nodeId Node Id
+     *        percentage the percentage the node owner select to pay
+     *        amountEnergy amount of the energy the Node consumed, in decimal of 6, 1kWh -> 1_000_000 
+     */
+    function buyNode(uint24 nodeId, uint8 percentage, uint64 amountEnergy, Sig calldata signature) public {
+        require (nodeInfo[nodeId].owner == address(0) , "GBC2: Node Sold");
+        require (percentage <= 100 , "GBC2: Wrong percentage");
+
+        bytes32 buyNodeHash = keccak256(abi.encode(BUY_NODE_TYPEHASH, msg.sender, nodeId, percentage, amountEnergy));
+        bytes32 digest = keccak256(abi.encodePacked('\x19\x01', _DOMAIN_SEPARATOR, buyNodeHash));
+        address managerAddress = ECDSAUpgradeable.recover(digest, signature.v, signature.r, signature.s);
+        require(managerAddress == luckyManager, "Wrong Signature");
+
+        nodeInfo[nodeId].owner = msg.sender;
+        nodeInfo[nodeId].nodeId = nodeId;
+        nodeInfo[nodeId].percentage = percentage;
+        nodeInfo[nodeId].amountEnergy = amountEnergy;
+        nodeSold += 1;
+
+        allNodeIds.concatStorage(abi.encodePacked(bytes3(uint24(nodeId))));
+
+        IkWhToken(kWhToken).transferFrom(msg.sender, address(this), amountEnergy);
+        emit BuyNode(nodeId, msg.sender, percentage, amountEnergy);
     }
 
     /**
@@ -168,6 +214,7 @@ contract GreenBTC2S is
         uint256 domainInfo = uint256(domains[domainID]);
         require ( domainInfo != 0 , "GBC2: Empty Domain");
 
+        uint24  nodeId = uint24(domainInfo >> 32);
         uint256 boxTop = uint32(domainInfo >> 192);
         uint256 boxMadeGreen = uint32(domainInfo);
         require (boxMadeGreen < boxTop, "GBC2: All Greenized");
@@ -178,20 +225,32 @@ contract GreenBTC2S is
         uint256 actionID = actionNumber + 1;
         actionNumber = actionID;
 
-        // blockHeight: MSB0:4; domainId: MSB4:2; boxStart: MSB6:4; boxAmount: MSB10: 2
-        bytes32 actionValue = bytes32((uint256(uint32(block.number)) << 224) 
-                            + (uint256(uint16(domainID)) << 208) + uint256(boxMadeGreen << 176) 
-                            + (uint256(uint16(boxSteps)) << 160) + uint256(uint160(msg.sender)));
-        greenActions[actionID] = actionValue;
+        {
+            // blockHeight: MSB0:4; domainId: MSB4:2; boxStart: MSB6:4; boxAmount: MSB10: 2
+            bytes32 actionValue = bytes32((uint256(uint32(block.number)) << 224) 
+                                + (uint256(uint16(domainID)) << 208) + uint256(boxMadeGreen << 176) 
+                                + (uint256(uint16(boxSteps)) << 160) + uint256(uint160(msg.sender)));
+            greenActions[actionID] = actionValue;
+        }
 
         userActionIDs[msg.sender].concatStorage(abi.encodePacked(bytes4(uint32(actionID))));
         domainActionIDs[domainID].concatStorage(abi.encodePacked(bytes4(uint32(actionID))));
         domains[domainID] = bytes32(((domainInfo >> 32) << 32) + (boxMadeGreen + boxSteps));
 
-        emit DomainGreenized(msg.sender, actionID, block.number, domainID, boxMadeGreen, boxStepsSaved);
-
         uint256 kWhAmount = boxSteps * DecimalMath.getDecimalPower(uint8(domainInfo >> 56) & 0x0F);     // convert to kWh
-        IkWhToken(kWhToken).burnFrom(msg.sender, kWhAmount);
+        
+        if (nodeId == 0) {
+            emit DomainGreenized(msg.sender, actionID, block.number, domainID, boxMadeGreen, boxStepsSaved);
+            IkWhToken(kWhToken).burnFrom(msg.sender, kWhAmount);
+        } else {
+            emit DomainGreenizedNode(msg.sender, nodeId, nodeInfo[nodeId].owner, nodeInfo[nodeId].percentage,
+                                        actionID, block.number, domainID, boxMadeGreen, boxStepsSaved);
+
+            uint256 kWhAmountOwner = kWhAmount * nodeInfo[nodeId].percentage / 100;
+            IkWhToken(kWhToken).burn(kWhAmountOwner);
+
+            IkWhToken(kWhToken).burnFrom(msg.sender, kWhAmount - kWhAmountOwner);
+        }
     }
 
     /**
@@ -213,6 +272,7 @@ contract GreenBTC2S is
         require ( domainInfo != 0 , "GBC2: Empty Domain");
         require (nonce == nonces[greener], "Nonce Not Match"); 
         require (greener == msg.sender, "GBC2: Not allowed");
+        require (uint24(domainInfo >> 32) == 0, "GBC2: Lucky Node not suppot");         // Check nodeId
 
         {
           bytes32 stakeHash = keccak256(abi.encode(LUCKY_TYPEHASH, domainID, boxSteps, greener, nonce, deadline));
@@ -248,6 +308,25 @@ contract GreenBTC2S is
         luckyFundInfo.amountDroped += uint96(kWhAmount);
 
         IkWhToken(kWhToken).burn(kWhAmount);
+    }
+
+    /**
+     * @dev Get the nodeIds
+     * @param offset offset of the nodeIds to get, starting from 0, each nodeId occupies 3 bytes
+     *        length number to nodeIds to get starting from offset, if = 0, get all the actionIDs till the end
+     * @return totalOfActions total of nodeIds returned.
+     * @return actionIds returned actionIDs, each in 3 bytes 
+     */
+    function getNodeIDs (uint256 offset, uint256 length) external view 
+            returns (uint256, bytes memory) 
+    {
+        uint256 lengthAllNodeIds = allNodeIds.length;
+        offset = 3 * offset;
+        length = 3 * length;
+
+        if (offset > lengthAllNodeIds) offset = lengthAllNodeIds;
+        if ((length == 0)  || ((offset + length) > lengthAllNodeIds)) length = lengthAllNodeIds - offset;
+        return (lengthAllNodeIds/3, allNodeIds.getStorage(offset, length));
     }
 
     /**
--- contracts/interfaces/IkWhToken.sol
+++ contracts/interfaces/IkWhToken.sol
@@ -6,6 +6,7 @@ interface IkWhToken {
     function burnFrom(address account, uint256 amount) external;
     function burn(uint256 amount) external;
     function convertKWh(address tokenToPay, uint256 amountPayment) external returns (uint256);
-
-
+    function transferFrom(address from,address to,uint256 amount) external returns (bool);
+    function transfer(address to, uint256 value) external returns (bool);
+    function balanceOf(address) external view returns (uint256);
 }
